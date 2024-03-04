fd6c67038102af63cffca9e19df72cc3237ff6f2
Shivam1320 <shivam.skt13@gmail.com>
2024-03-04 18:23:01 +0530

    added featured, experience and galllery section in Home Page

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { Card, CardBody } from "reactstrap";
 import { Link } from "react-router-dom";
 import "./tour-card.css";
+import calculateAvgRating from '../utils/avgRating';
 
 const TourCard = ({ tour }) => {
-    const {id, title, city, photo, price, featured, avgRating, reviews } = tour;
+    const {id, title, city, photo, price, featured, reviews } = tour;
+
+    const {totalRating, avgRating} = calculateAvgRating(reviews);
 
   return (
     <div className="tour__card">
         <Card>
             <div className="tour__img">
                 <img src={photo} alt="tour-img" />
-                <span>Featured</span>
+                {featured && <span>Featured</span>}
             </div>
 
             <CardBody>
@@ -21,8 +24,8 @@ const TourCard = ({ tour }) => {
                 </span>
                 
                 <span className='tour__rating d-flex align-items-center gap-1'>
-                    <i class = "ri-star-fill"></i> {avgRating}{" "}
-                    <span>({reviews.length})</span>
+                    <i class = "ri-star-fill"></i> {avgRating === 0 ? null : avgRating}
+                    {totalRating === 0 ? ( 'Not rated' ) : (<span>({reviews.length})</span>)}
                 </span>
             </div>
 
